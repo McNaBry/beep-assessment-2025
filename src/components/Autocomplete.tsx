5e50160d6510683e3f58c48be1834321df48d7f6
McNaBry <mcnabry123@gmail.com>
2025-04-06 16:37:31 +0800

    Add option to delete by clicking option chip

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -215,10 +215,19 @@ export default function Autocomplete({
           { multiple && (selected as Option[]).length > 0 &&
             <>
               { (selected as Option[]).map((selectedOption) => (
-                  <div key={getStringValue(selectedOption)} className="w-max flex items-center bg-gray-300 text-gray-800 text-sm px-3 py-1 rounded-full">
+                  <div 
+                    key={getStringValue(selectedOption)} 
+                    className="w-max flex gap-1 justify-between items-center bg-gray-300 text-gray-800 text-sm px-3 py-1 rounded-full"
+                  >
                     <span>
                       { getOptionLabel ? getOptionLabel(selectedOption) : getStringValue(selectedOption) }
                     </span>
+                    <div 
+                      onClick={() => onSelectedChange(selectedOption)}
+                      className="cursor-pointer"
+                    >
+                      <CrossFilled height={16} width={16} className="hover:text-red-800" />
+                    </div>
                   </div>
                 ))
               }
--- src/components/CrossFilled.tsx
+++ src/components/CrossFilled.tsx
@@ -1,11 +1,14 @@
+import { SVGProps } from "react";
+
 // Taken from https://reactsvgicons.com/
-export function CrossFilled() {
+export function CrossFilled(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       width="20"
       height="20"
+      {...props}
     >
       <g fill="currentColor" fillRule="evenodd" clipRule="evenodd">
         <path d="M5.47 5.47a.75.75 0 0 1 1.06 0l12 12a.75.75 0 1 1-1.06 1.06l-12-12a.75.75 0 0 1 0-1.06"></path>
